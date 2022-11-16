d8f14bcae9768eff5491a0be7aaec0691331740d
Caleb <calebfunderburk@icloud.com>
2022-11-16 15:07:15 -0500

    started idb integration

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,15 +1,16 @@
-import { Tooltip, Toast, Popover } from 'bootstrap'
 import './form'
 import './submit'
 import '../css/index.css'
+import { Tooltip, Toast, Popover } from 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Logo from '../images/logo.png'
 import Bear from '../images/bear.png'
 import Dog from '../images/dog.png'
+import { initDb, getDb, postDb } from './database'
 
 window.addEventListener('load', function() {
+    initDb()
     document.getElementById('logo').src = Logo
     document.getElementById('bearThumbnail').src = Bear
     document.getElementById('dogThumbnail').src = Dog
-
 })
